39ef61ae97326668d83745efd487230e17682dc4
João Alonso <joaoalonso42@gmail.com>
2020-12-12 20:06:12 -0300

    FirstGame
    first step

---
+++ guessingGame/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+Rules
+The computer will think of a number, and you, the player, will need guess it.
+Which each error, the machine will tell you whether the number kicked was greater or lower than thought.
+You also be able to choose the difficulty level of the game, that will give you more or less opportunities to kick a number. 
+At the end, if you win, the computer will tell you know how many points you have made, based on in how good your kicks were.
+  */
+
+int main(){
+    printf("******************************");
+    printf("\n*welcome to the guessing game*");
+    printf("\n******************************");
+
+}
+ //Presentation 
+
+ //choise of difficulty
+
+ //start oh the game
+
+ //end of the game
