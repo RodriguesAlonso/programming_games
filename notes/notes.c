717a8d996a06110c067942cd58e7798dd07cc94d
João Alonso <joaoalonso42@gmail.com>
2021-02-17 15:35:40 -0300

    notes2

---
+++ notes/notes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* printf("%d", &var);
+%f, %c, %s*/
+
+/*scanf("%d", &var);*/
+
+/*
+#include <time.h>// random
+    srand(time(0));
+    int random = rand() % 10;
+    printf("%d", random);   
+*/
+
+int main()
+{
+    int nun;
+    printf("digite um número: \n");    
+    scanf("%d", &nun);
+    return 0;
+}
+
